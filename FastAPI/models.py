9512257d26435695ffea5da362ebf6f7b651457d
Aman Sah <amansah1717@gmail.com>
2025-08-30 19:46:47 +0530

    First commit

---
+++ FastAPI/models.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, Float
+
+
+
+class Transactions(Base):
+
+    __tablename__ = 'transactions'
+
+    id =  Column(Integer, primary_key= True, index= True)
+
+    amount = Column(Float)
+    categoory = Column(String)  
+    description = Column(String)
+    is_income =  Column(Boolean)
+    date = Column(String)
